bb46184f86f1c61298bd68cb82655cd92e9b3d8d
Gabor Javorszky <gabor@suborbital.dev>
2023-03-14 11:29:33 +0000

    Remove unused / old version of client

--- client.go
+++ client.go
@@ -4,18 +4,11 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
-	"io"
 	"net/http"
 	"time"
 
 	"github.com/pkg/errors"
 )
-
-// Client is used for interacting with the SE2 API
-type Client struct {
-	config   *Config
-	envToken string
-}
 
 const (
 	modeUnset ServerMode = iota
@@ -121,62 +114,4 @@ func (c *Client2) do(ctx context.Context, req *http.Request) (*http.Response, er
 	req.Header.Set("Authorization", "Bearer "+c.token)
 
 	return c.httpClient.Do(req)
-}
-
-// NewClient creates a Client with a Config
-func NewClient(config *Config, envToken string) (*Client, error) {
-	if config == nil {
-		return nil, errors.New("failed to NewClient: config cannot be nil")
-	}
-
-	client := &Client{
-		config:   config,
-		envToken: envToken,
-	}
-
-	return client, nil
-}
-
-// NewLocalClient quickly sets up a Client with a LocalConfig. Useful for testing.
-func NewLocalClient(envToken string) (*Client, error) {
-	return NewClient(LocalConfig(), envToken)
-}
-
-func (c *Client) adminRequestBuilder(method string, endpoint string, body io.Reader) (*http.Request, error) {
-	url := *c.config.adminURL
-	url.Path = endpoint
-
-	return http.NewRequest(method, url.String(), body)
-}
-
-func (c *Client) execRequestBuilder(method string, endpoint string, body io.Reader) (*http.Request, error) {
-	url := *c.config.executionURL
-	url.Path = endpoint
-
-	return http.NewRequest(method, url.String(), body)
-}
-
-func (c *Client) builderRequestBuilder(method string, endpoint string, body io.Reader) (*http.Request, error) {
-	url := *c.config.builderURL
-	url.Path = endpoint
-
-	return http.NewRequest(method, url.String(), body)
-}
-
-func (c *Client) do(req *http.Request) (*http.Response, error) {
-	res, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		if res != nil && res.Body != nil {
-			res.Body.Close()
-		}
-		return nil, err
-	}
-
-	if res.StatusCode < 200 || res.StatusCode > 299 {
-		res.Body.Close()
-		return nil, errors.Errorf("API returned non-successful status: %s", res.Status)
-	}
-
-	return res, nil
 }
--- execution_requests.go
+++ execution_requests.go
@@ -3,11 +3,9 @@ package se2
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"path"
 
 	"github.com/pkg/errors"
 )
@@ -43,64 +41,4 @@ func (c *Client2) Exec(ctx context.Context, payload []byte, ident, namespace, pl
 	}
 
 	return b, nil
-}
-
-// execPlugin remotely executes the provided plugin using the body as input. See also: ExecString()
-func (c *Client) execPlugin(endpoint string, body io.Reader) ([]byte, string, error) {
-	req, err := c.execRequestBuilder(http.MethodPost, endpoint, body)
-	req.Header.Set("Authorization", "Bearer "+c.envToken)
-
-	if err != nil {
-		return nil, "", err
-	}
-
-	res, err := c.do(req)
-	if err != nil && res == nil {
-		return nil, "", err
-	}
-	defer res.Body.Close()
-
-	result, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, "", err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		errRes := ExecError{}
-		err = json.Unmarshal(result, &errRes)
-		if err != nil {
-			return nil, "", err
-		}
-		newErr := errors.Errorf("[%d]: %s", errRes.Code, errRes.Message)
-
-		return nil, "", newErr
-	}
-
-	return result, res.Header.Get("x-suborbital-requestid"), nil
-}
-
-// Exec remotely executes the provided plugin using the body as input. See also: ExecString()
-func (c *Client) Exec(plugin *Plugin, body io.Reader) ([]byte, string, error) {
-	if plugin == nil {
-		return nil, "", errors.New("Plugin cannot be nil")
-	}
-
-	return c.execPlugin(path.Join("/name", plugin.URI()), body)
-}
-
-// ExecString sets up a buffer with the provided string and calls Exec
-func (c *Client) ExecString(plugin *Plugin, body string) ([]byte, string, error) {
-	buf := bytes.NewBufferString(body)
-	return c.Exec(plugin, buf)
-}
-
-// ExecRef remotely executes the provided plugin using the body as input, by plugin reference. See also: ExecRefString()
-func (c *Client) ExecRef(ref string, body io.Reader) ([]byte, string, error) {
-	return c.execPlugin(path.Join("/ref", ref), body)
-}
-
-// ExecRefString sets up a buffer with the provided string and calls ExecRef
-func (c *Client) ExecRefString(ref string, body string) ([]byte, string, error) {
-	buf := bytes.NewBufferString(body)
-	return c.ExecRef(ref, buf)
 }
